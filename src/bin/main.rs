c4928a000de3af285db95a89da9cae843d4aa220
Valentin Wyss <wyssvalentin@yahoo.fr>
2023-06-14 07:35:31 +0200

    Fix deadlock

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,24 +1,33 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+mod database;
 mod windows;
+
+use crate::database::connection::establish_connection;
+use tokio::runtime::Runtime;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, Registry};
+use windows::main_window::MainWindow;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
-    use tokio::runtime::Runtime;
-    use windows::main_window::MainWindow;
+    use tracing_subscriber::fmt::format::FmtSpan;
 
     let rt = Runtime::new().expect("Unable to create Runtime");
-
-    // Enter the runtime so that `tokio::spawn` is available immediately.
     let _enter = rt.enter();
 
-    let native_options = eframe::NativeOptions::default();
+    let stdout_log = tracing_subscriber::fmt::layer()
+        .with_span_events(FmtSpan::ACTIVE)
+        .pretty();
+    let subscriber = Registry::default().with(stdout_log);
+
+    tracing::subscriber::set_global_default(subscriber).expect("Unable to set global subscriber");
+
     eframe::run_native(
         "Dofus farmer",
-        native_options,
-        Box::new(|cc| Box::new(MainWindow::new(cc))),
+        Default::default(),
+        Box::new(|cc| Box::new(MainWindow::new(cc, establish_connection()))),
     )
 }
 
--- src/bin/windows/main_window.rs
+++ src/bin/windows/main_window.rs
@@ -3,15 +3,26 @@ use std::{
     sync::mpsc::{Receiver, Sender},
 };
 
+use diesel::PgConnection;
 use egui::{
-    CentralPanel, Color32, ColorImage, Context, Frame, Pos2, Rect, TextureHandle, Ui, Vec2, Window,
+    CentralPanel, Color32, ColorImage, Context, Frame, Pos2, Rect, Rounding, TextureHandle, Ui,
+    Vec2, Window,
 };
 use image::io::Reader;
 use lombok::AllArgsConstructor;
+use tracing::{span, trace_span, Level};
 
-pub enum ImageStatus {
+enum ImageStatus {
     Loading,
     Ready(Image),
+}
+
+#[derive(AllArgsConstructor)]
+pub struct MapMinMax {
+    x_min: i16,
+    x_max: i16,
+    y_min: i16,
+    y_max: i16,
 }
 
 #[derive(AllArgsConstructor)]
@@ -28,7 +39,7 @@ impl Image {
             zoom,
             index + 1
         ));
-        let handle = ctx.load_texture(&format!("map{index}"), color_image, Default::default());
+        let handle = ctx.load_texture(format!("map{index}"), color_image, Default::default());
 
         Image::new(handle, true)
     }
@@ -48,9 +59,11 @@ pub struct MainWindow {
     position: Pos2,
     clicked_position: Option<Pos2>,
     images: HashMap<(u16, usize), ImageStatus>,
-    image_number: (u8, u8),
+    images_number: (u8, u8),
+    map_min_max: MapMinMax,
     tx: Sender<(Image, u16, usize)>,
     rx: Receiver<(Image, u16, usize)>,
+    connection: PgConnection,
 }
 
 impl MainWindow {
@@ -58,27 +71,77 @@ impl MainWindow {
     const FULL_IMAGE_SIZE: Vec2 = Vec2::new(10000f32, 8000f32);
     const ZOOMS: [f32; 5] = [0.2, 0.4, 0.6, 0.8, 1f32];
     const STARTING_ZOOM_INDEX: usize = 0;
+    const MAPS_RECT: Rect = Self::init_map_rect();
 
-    pub fn new(_: &eframe::CreationContext<'_>) -> Self {
-        Default::default()
+    const fn init_map_rect() -> Rect {
+        let min = Pos2::new(0f32, 0f32);
+        let max = Pos2::new(
+            (min.x as i32 + Self::FULL_IMAGE_SIZE.x as i32) as f32,
+            (min.y as i32 + Self::FULL_IMAGE_SIZE.y as i32) as f32,
+        );
+
+        Rect { min, max }
     }
 
-    fn body_loop(&mut self, x: i32, y: i32, pos: Pos2, ui: &Ui) {
+    pub fn new(_: &eframe::CreationContext<'_>, mut connection: PgConnection) -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        let zoom_index = Self::STARTING_ZOOM_INDEX;
+        let images_number = Self::create_zoom(zoom_index);
+        let map_min_max = {
+            use crate::database::schema::maps;
+            use diesel::dsl::{max, min};
+            use diesel::prelude::*;
+
+            let min_max = maps::table
+                .select((
+                    min(maps::x).assume_not_null(),
+                    max(maps::x).assume_not_null(),
+                    min(maps::y).assume_not_null(),
+                    max(maps::y).assume_not_null(),
+                ))
+                .first::<(i16, i16, i16, i16)>(&mut connection)
+                .unwrap();
+
+            MapMinMax::new(
+                min_max.0 as _,
+                min_max.1 as _,
+                min_max.2 as _,
+                min_max.3 as _,
+            )
+        };
+
+        Self {
+            zoom_index,
+            position: Pos2::ZERO,
+            clicked_position: None,
+            images: HashMap::new(),
+            images_number,
+            map_min_max,
+            tx,
+            rx,
+            connection,
+        }
+    }
+
+    fn draw_map_body_loop(&mut self, x: i32, y: i32, pos: Pos2, ui: &Ui) {
+        let span = trace_span!("draw_map_body_loop");
+        let _guard = span.enter();
+
         let new_x = x - pos.x as i32;
         let new_y = y - pos.y as i32;
 
         let x_index = (new_x as f32 / Self::IMAGE_SIZE.x).floor() as i8;
         let y_index = (new_y as f32 / Self::IMAGE_SIZE.y).floor() as i8;
 
-        if (0..self.image_number.0 as i8).contains(&x_index)
-            && (0..self.image_number.1 as i8).contains(&y_index)
+        if (0..self.images_number.0 as i8).contains(&x_index)
+            && (0..self.images_number.1 as i8).contains(&y_index)
         {
-            let index = y_index as u16 * self.image_number.0 as u16 + x_index as u16;
+            let index = y_index as u16 * self.images_number.0 as u16 + x_index as u16;
             if let Some(image_status) = self.images.get_mut(&(index, self.zoom_index)) {
                 if let ImageStatus::Ready(image) = image_status {
                     let pos = Pos2::new(x as f32, y as f32);
 
-                    // println!("drawing {} in {:?}", image.name(), pos);
                     ui.painter().image(
                         image.handle.id(),
                         Rect::from_two_pos(pos, pos + Self::IMAGE_SIZE),
@@ -97,10 +160,22 @@ impl MainWindow {
     }
 
     fn central_panel_ui(&mut self, ui: &Ui) {
+        let span = trace_span!("central_panel_ui");
+        let _guard = span.enter();
+
         let ctx = ui.ctx();
-        ui.input(|input_state| {
-            if ui.ui_contains_pointer() {
+        let ui_contains_pointer = ui.ui_contains_pointer();
+        let pointer_pos = ui.input(|input_state| {
+            let span = trace_span!("read inputs");
+            let _guard = span.enter();
+
+            if ui_contains_pointer {
+                let span = trace_span!("ui_contains_pointer");
+                let _guard = span.enter();
+
                 if input_state.pointer.primary_pressed() {
+                    let span = trace_span!("primary_pressed");
+                    let _guard = span.enter();
                     self.clicked_position = input_state.pointer.interact_pos();
                 }
 
@@ -120,17 +195,27 @@ impl MainWindow {
             }
 
             if input_state.pointer.primary_released() {
+                let span = trace_span!("primary_released");
+                let _guard = span.enter();
                 if let Some(clicked_position) = self.clicked_position {
-                    self.position += ctx.pointer_interact_pos().unwrap() - clicked_position;
+                    let span = trace_span!("if");
+                    let _guard = span.enter();
+                    self.position += input_state.pointer.interact_pos().unwrap() - clicked_position;
                     self.clicked_position = None;
                 }
             }
+
+            input_state
+                .pointer
+                .interact_pos()
+                .filter(|_| ui_contains_pointer)
         });
 
+        // Draw full map images
         let pos = self.position
             + self
                 .clicked_position
-                .map(|pos| ctx.pointer_interact_pos().unwrap() - pos)
+                .map(|pos| ctx.pointer_latest_pos().unwrap() - pos)
                 .unwrap_or(Vec2::ZERO);
 
         self.images.iter_mut().for_each(|(_, image_status)| {
@@ -139,10 +224,8 @@ impl MainWindow {
             }
         });
 
-        let top_left = Pos2::ZERO;
-
         let size = ui.available_size();
-        let rect = Rect::from_two_pos(top_left, top_left + size);
+        let rect = Rect::from_two_pos(Pos2::ZERO, Pos2::ZERO + size);
 
         let left = pos.x % Self::IMAGE_SIZE.x;
         let top = pos.y % Self::IMAGE_SIZE.y;
@@ -150,7 +233,7 @@ impl MainWindow {
         let bottom = rect.bottom();
         for x in (left as i32..=right as i32).step_by(Self::IMAGE_SIZE.x as usize) {
             for y in (top as i32..=bottom as i32).step_by(Self::IMAGE_SIZE.y as usize) {
-                self.body_loop(x, y, pos, ui);
+                self.draw_map_body_loop(x, y, pos, ui);
             }
         }
 
@@ -160,13 +243,33 @@ impl MainWindow {
             }
             true
         });
+
+        // Draw map rect on pointer
+        if let Some(pointer_pos) = pointer_pos {
+            let rect_size = Vec2::new(
+                Self::MAPS_RECT.width() / (self.map_min_max.x_max - self.map_min_max.x_min) as f32,
+                Self::MAPS_RECT.height() / (self.map_min_max.y_max - self.map_min_max.y_min) as f32,
+            );
+            let rect = Rect::from_center_size(pointer_pos, rect_size);
+            ui.painter().rect_filled(
+                rect,
+                Rounding::none(),
+                Color32::from_rgba_unmultiplied(61, 183, 255, 100),
+            )
+        }
     }
 
     fn load_image(&mut self, ctx: Context, index: u16) {
+        let span = trace_span!("load_image");
+        let _guard = span.enter();
+
         let tx = self.tx.clone();
         let zoom_index = self.zoom_index;
         let zoom = Self::ZOOMS[zoom_index];
         tokio::spawn(async move {
+            let span = trace_span!("load_image inner async");
+            let _guard = span.enter();
+
             let image = Image::from_ui_and_index(&ctx, index, zoom);
             let _ = tx.send((image, index, zoom_index));
             ctx.request_repaint();
@@ -188,7 +291,7 @@ impl MainWindow {
     fn update_zoom(&mut self, zoom_index: usize) {
         self.images.clear();
         self.zoom_index = zoom_index;
-        self.image_number = Self::create_zoom(zoom_index);
+        self.images_number = Self::create_zoom(zoom_index);
     }
 
     fn create_zoom(zoom_index: usize) -> (u8, u8) {
@@ -200,30 +303,12 @@ impl MainWindow {
     }
 }
 
-impl Default for MainWindow {
-    fn default() -> Self {
-        let (tx, rx) = std::sync::mpsc::channel();
-
-        let zoom_index = Self::STARTING_ZOOM_INDEX;
-        let image_number = Self::create_zoom(zoom_index);
-
-        println!("{image_number:?}");
-
-        Self {
-            zoom_index,
-            position: Pos2::ZERO,
-            clicked_position: None,
-            images: HashMap::new(),
-            image_number,
-            tx,
-            rx,
-        }
-    }
-}
-
 impl eframe::App for MainWindow {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let images_loaded_length = self
+        let span = trace_span!("update");
+        let _guard = span.enter();
+
+        let _images_loaded_length = self
             .images
             .iter()
             .filter(|(_, image_status)| {
@@ -235,7 +320,7 @@ impl eframe::App for MainWindow {
             })
             .count();
 
-        let images_loading_length = self
+        let _images_loading_length = self
             .images
             .iter()
             .filter(|(_, image_status)| {
@@ -247,13 +332,17 @@ impl eframe::App for MainWindow {
             })
             .count();
 
-        // println!("{images_loaded_length} images loaded, {images_loading_length} images loading");
-        self.rx.try_iter().for_each(|(image, index, zoom_index)| {
-            if zoom_index == self.zoom_index {
-                self.images
-                    .insert((index, self.zoom_index), ImageStatus::Ready(image));
-            }
-        });
+        // println!("{_images_loaded_length} images loaded, {_images_loading_length} images loading");
+        {
+            let span = trace_span!("iterate new images with status Ready");
+            let _guard = span.enter();
+            self.rx.try_iter().for_each(|(image, index, zoom_index)| {
+                if zoom_index == self.zoom_index {
+                    self.images
+                        .insert((index, self.zoom_index), ImageStatus::Ready(image));
+                }
+            });
+        }
 
         let frame = Frame::default().fill(Color32::from_rgb(30, 25, 25));
         CentralPanel::default()
